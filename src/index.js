55c483b145b9443b31eb4e36acf719c4d2375a43
Keith Charles <keithc@ouraysportswear.com>
2020-11-27 19:02:14 -0700

    test authProvider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { AuthProvider } from './contexts/AuthContext';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Wrappers = () => (
+  <AuthProvider>
+    <Suspense fallback={<h3>Loading...</h3>}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Suspense>
+  </AuthProvider>
+);
+
+ReactDOM.render(<Wrappers />, document.getElementById('root'));
