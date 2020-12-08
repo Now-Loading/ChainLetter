d66fdf74681ab525b9d1d4b05110ab92ae18f017
Keith Charles <keithc@ouraysportswear.com>
2020-12-08 08:18:14 -0700

    add story submission functionality

--- src/components/templates/AddStory.js
+++ src/components/templates/AddStory.js
@@ -1,23 +1,60 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../organisms/Modal';
+import { db } from '../../firebase';
+import { useAuthContext } from '../../contexts/AuthContext';
 
 const AddStory = ({ toggleModal }) => {
+  const { currentUser } = useAuthContext();
+  // pull values from localStorage if they exist
   const [newStory, setNewStory] = useState({
-    title: localStorage.getItem('new-title') || '',
-    body: localStorage.getItem('new-body') || '',
+    title: localStorage.getItem('story-title') || '',
+    content: localStorage.getItem('story-content') || '',
   });
 
+  /**
+   * Attempts to add story to firestore when user clicks submit
+   * @param {Object} e - event that is injected from onSubmit
+   */
+  const addNewStory = async (e) => {
+    e.preventDefault();
+    // don't attempt if no values in the form fields
+    if (
+      !newStory.title.trim()
+      || !newStory.content.trim()
+    ) return;
+
+    try {
+      await db.collection('links')
+        .add({
+          title: newStory.title,
+          content: newStory.content,
+          author: currentUser.displayName,
+        });
+      localStorage.removeItem('story-title');
+      localStorage.removeItem('story-content');
+    } catch (err) {
+      console.log(err);
+      throw err;
+    } finally {
+      toggleModal(false);
+    }
+  };
+
+  /**
+   * Saves values of field to localStorage whenever user changes field
+   * @param {Object} e - event that is injected from onSubmit
+   */
   const handleStoryContentChange = (e) => {
     const { name, value } = e.target;
     if (name === 'title' && value.length > 75) return;
-    if (name === 'body' && value.length > 240) return;
+    if (name === 'content' && value.length > 240) return;
 
     setNewStory((prev) => ({
       ...prev,
       [name]: value,
     }));
-    localStorage.setItem(`new-${name}`, value);
+    localStorage.setItem(`story-${name}`, value);
   };
 
   return (
@@ -26,7 +63,7 @@ const AddStory = ({ toggleModal }) => {
       subTitle="write the first lines of a brand new story"
       confirmText="Submit Story"
       canCancel
-      submitHandler={() => {}}
+      submitHandler={addNewStory}
       cancelHandler={() => toggleModal(false)}
     >
       <label htmlFor="story-add-title">
@@ -47,14 +84,14 @@ const AddStory = ({ toggleModal }) => {
         </div>
         <textarea
           id="story-add-content"
-          name="body"
-          value={newStory.body}
+          name="content"
+          value={newStory.content}
           onChange={handleStoryContentChange}
         />
       </label>
       <span>
         Characters Left:
-        {240 - newStory.body.length}
+        {240 - newStory.content.length}
       </span>
     </Modal>
   );
