28449860cbdce5b78aa4612b117cbdd69e11858f
Keith Charles <keithc@ouraysportswear.com>
2020-12-08 23:04:17 -0700

    format stories from db calls

--- src/components/organisms/StoryList.js
+++ src/components/organisms/StoryList.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import PropType from 'prop-types';
+import './StoryList.scss';
 
-const StoryList = ({
-  stories,
-}) => {
-  const storyItems = () => (
-    stories.map((story, i) => <li key={`item-${Math.random(i)}`}>{story.content}</li>)
-  );
-
-  return (
-    <ul>
-      {storyItems()}
-    </ul>
-  );
-};
+const StoryList = ({ stories }) => (
+  <section className="story-list">
+    {
+      stories.map((story) => (
+        <article key={story.id}>
+          <header>
+            <h3>{story.title}</h3>
+            <span className="byline">
+              by&nbsp;
+              {story.author}
+            </span>
+          </header>
+          <p>
+            {story.content}
+          </p>
+        </article>
+      ))
+    }
+  </section>
+);
 
 export default StoryList;
 
