183a7208ce148df8cd12256710e827dca28df735
Keith Charles <keith.k.charles@gmail.com>
2020-12-17 17:51:15 -0700

    add a buttons prop to modal

--- src/components/organisms/Modal.js
+++ src/components/organisms/Modal.js
@@ -7,26 +7,30 @@ const Modal = ({
   title,
   subTitle,
   children,
+  buttons,
   submitHandler,
   cancelHandler,
   confirmText,
   cancelText,
-  canCancel,
 }) => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    submitHandler(e);
-  };
-
-  const handleCancel = () => {
-    cancelHandler();
+  const handleCancel = (event) => {
+    if (!cancelHandler) {
+      return;
+    }
+    cancelHandler(event);
   };
 
   return (
     <div
       className="modal"
     >
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={
+          submitHandler
+            ? submitHandler()
+            : (event) => event.preventDefault()
+        }
+      >
         <header>
           {title && <h3>{title}</h3>}
           {subTitle && <h4>{subTitle}</h4>}
@@ -35,7 +39,7 @@ const Modal = ({
           {children}
         </main>
         <footer>
-          {canCancel && (
+          {cancelHandler && (
             <Button
               type="button"
               text={cancelText}
@@ -43,11 +47,14 @@ const Modal = ({
               clickHandler={handleCancel}
             />
           )}
-          <Button
-            type="submit"
-            text={confirmText}
-            variant="primary"
-          />
+          {buttons}
+          {submitHandler && (
+            <Button
+              type="submit"
+              text={confirmText}
+              variant="primary"
+            />
+          )}
         </footer>
       </form>
     </div>
@@ -60,20 +67,20 @@ Modal.propTypes = {
   title: PropTypes.string,
   subTitle: PropTypes.string,
   children: PropTypes.arrayOf(PropTypes.element),
+  buttons: PropTypes.arrayOf(PropTypes.element),
   submitHandler: PropTypes.func,
   cancelHandler: PropTypes.func,
   confirmText: PropTypes.string,
   cancelText: PropTypes.string,
-  canCancel: PropTypes.bool,
 };
 
 Modal.defaultProps = {
   title: null,
   subTitle: null,
   children: null,
+  buttons: [],
   submitHandler: null,
   cancelHandler: null,
   confirmText: 'Confirm',
   cancelText: 'Cancel',
-  canCancel: false,
 };
