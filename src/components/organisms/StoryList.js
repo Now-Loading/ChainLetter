bb1875796770de2615301bed163b7450fdcdbc2f
Conor <67439130+conorkenahan@users.noreply.github.com>
2021-01-20 19:36:01 -0500

    userName is now displaying from currentUser from AuthContext

--- src/components/organisms/StoryList.js
+++ src/components/organisms/StoryList.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import PropType from 'prop-types';
 import LinkCard from './LinkCard';
+import { useAuthContext } from '../../contexts/AuthContext';
+
 import './StoryList.scss';
 
-const StoryList = ({ stories }) => (
-  <div className="story-list-component">
-    <section className="story-list">
-      {
-        stories.map((story) => (
-          <LinkCard story={story} key={story.id} />
-        ))
-      }
-    </section>
-    <article className="user-info-panel">
-      <h3>author name</h3>
-    </article>
-  </div>
-);
+const StoryList = ({ stories }) => {
+  const { currentUser } = useAuthContext();
+
+  return (
+    <div className="story-list-component">
+      <section className="story-list">
+        {
+          stories.map((story) => (
+            <LinkCard story={story} key={story.id} />
+          ))
+        }
+      </section>
+      <article className="user-info-panel">
+        <h3>{currentUser.displayName}</h3>
+      </article>
+    </div>
+  );
+};
 
 export default StoryList;
 
