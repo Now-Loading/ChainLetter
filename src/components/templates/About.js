4e1de21b08938a8800b3a2df734df9f56f842f49
Conor <67439130+conorkenahan@users.noreply.github.com>
2021-02-22 14:10:56 -0500

    CSS styling added

--- src/components/templates/About.js
+++ src/components/templates/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const About = () => (
-  <div>
+  <div className="about">
     <h1>About</h1>
     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit...</p>
   </div>
